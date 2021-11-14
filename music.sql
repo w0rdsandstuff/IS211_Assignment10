229aaa7674101865ae75ce3e35d96a10d504c8b5
w0rdsandstuff <77218212+w0rdsandstuff@users.noreply.github.com>
2021-11-14 12:35:13 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,23 @@
 
+CREATE TABLE artists ( 
+  id INTEGER PRIMARY KEY,
+  artist_name TEXT);
+ 
+CREATE TABLE albums (
+  id INTEGER PRIMARY KEY,
+  album_name TEXT,
+  artist_name TEXT);
+  
+CREATE TABLE songs (
+  id INTEGER PRIMARY KEY,
+  song_name TEXT,
+  album_name TEXT,
+  track_number INTEGER,
+  track_length INTEGER);
+  
+  
+
+  
+  
+ 
+        
